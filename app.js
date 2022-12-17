8c9a937788f4be43e129f1736e9d33f3231628dc
devika <devikavarshney0003@gmail.com>
2022-12-17 18:49:57 +0530

    added styling to the website

--- app.js
+++ app.js
@@ -4,25 +4,24 @@ const bodyParser = require("body-parser");
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
-var items = ["Buy food", "Cook food"];
+app.use(express.static("public"));
+let items = ["Buy food", "Cook food"];
 
 
 app.get("/", function (req, res) {
-  var today = new Date();
-  var day = "";
-  var options={
+  let today = new Date();
+  let options={
     weekday:"long",
     day:"numeric",
     month:"long"
   }
-  var day = today.toLocaleDateString("en-US",options);
+  let day = today.toLocaleDateString("en-US",options);
   res.render("list",{kindOfDay:day, newListItems:items});
 });
 
 app.post("/", function(req,res){
-    var item = req.body.newItem;
+    let item = req.body.newItem;
     items.push(item);
-    console.log(item);
     res.redirect("/");
 })
 
