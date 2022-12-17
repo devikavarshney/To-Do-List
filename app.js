4463d463301b35cc58381f291ea0bd2560b70ecb
devika <devikavarshney0003@gmail.com>
2022-12-17 15:00:39 +0530

    adding new item to to do list added

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
-app.use('view engine','ejs');
 
-app.get("/", function(req,res){
-    var today = new Date();
-    if(today.getDay()===6 || today.getDay()===0){
-        res.send("<h2>Yay! It's weekend!</h2>");
-    }
-    else{
-        res.sendFile(__dirname+"/index.html");
-    }
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+var items = ["Buy food", "Cook food"];
+
+
+app.get("/", function (req, res) {
+  var today = new Date();
+  var day = "";
+  var options={
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+  }
+  var day = today.toLocaleDateString("en-US",options);
+  res.render("list",{kindOfDay:day, newListItems:items});
 });
 
-app.listen(3000,function(){
-    console.log("Server started on port 3000");
+app.post("/", function(req,res){
+    var item = req.body.newItem;
+    items.push(item);
+    console.log(item);
+    res.redirect("/");
+})
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
 });
