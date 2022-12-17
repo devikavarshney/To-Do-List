269c6fe5be447432a555bbb456a5d08e51d10b35
devika <devikavarshney0003@gmail.com>
2022-12-17 19:37:45 +0530

    refactor code by ading module and shifting date finder function to that module. Also added header and footer ejs files

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const res = require("express/lib/response");
+const date = require(__dirname+"/date.js");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 let items = ["Buy food", "Cook food"];
-
+let workItems =[];
 
 app.get("/", function (req, res) {
-  let today = new Date();
-  let options={
-    weekday:"long",
-    day:"numeric",
-    month:"long"
-  }
-  let day = today.toLocaleDateString("en-US",options);
-  res.render("list",{kindOfDay:day, newListItems:items});
+  res.render("list",{listTitle:date.getDate(), newListItems:items});
 });
 
 app.post("/", function(req,res){
+    
     let item = req.body.newItem;
+    if(req.body.list==="Work"){
+    workItems.push(item);
+    res.redirect("/work");
+    }
+    else{
     items.push(item);
     res.redirect("/");
-})
+    }
+});
+
+app.get("/work", function(req,res){
+    res.render("list",{listTitle:"Work List", newListItems:workItems});
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
